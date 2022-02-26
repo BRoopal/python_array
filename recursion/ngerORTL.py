18e0c2d9c65567cc4561420e2ae9be5bea73d0bd
BRoopal <ropal.bharti@gmail.com>
2022-02-26 05:48:30 -0800

    update ngerORTL

--- recursion/ngerORTL.py
+++ recursion/ngerORTL.py
@@ -31,4 +31,22 @@ ngerORTL(arr,n)
 3,  9
 1,  3
 8,  9
-5,  8"""
+5,  8
+
+LOGIC:
+
+#input :     5 8 1 3 9   4  5  2
+#output:     8 9 3 9 -1  5 -1 -1
+ pop answer pull
+r to l:
+stack     curr      pop         ans        push
+2                                -1
+2           5        2           -1        5
+5           4                     5        4
+5,4         9        4,5         -1        9
+9           3                     9        3
+9,3         1                     3        1
+9,3,1       8        1,3          9        8
+9,8         5                     8        5
+9,8,5
+"""
